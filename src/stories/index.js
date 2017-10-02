a1d428a8e66f82257f153e7607c644214de5b8af
Johnny Zhang <fd.zhonglin@gmail.com>
2017-10-01 21:15:56 -0700

    feat: Add component EditPost

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,8 +6,13 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+import EditPost from '../components/EditPost';
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('EditPost', module)
+  .add('default', () => <EditPost />);
