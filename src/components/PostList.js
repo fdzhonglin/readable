d713578ab230fa33e7e99ca62e1673e49e0896af
Johnny Zhang <fd.zhonglin@gmail.com>
2017-11-09 00:29:03 -0800

    feat: add component PostList

---
+++ src/components/PostList.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import _ from 'lodash';
+
+import Post from './Post';
+
+const PostListContainer = styled.div`
+`;
+
+export default function PostList({ posts }) {
+  return (
+    <PostListContainer>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} key={post.id} />
+      ))}
+    </PostListContainer>
+  )
+}
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape),
+};
+
+PostList.defaultProps = {
+  posts: [],
+};
--- src/stories/index.js
+++ src/stories/index.js
@@ -8,6 +8,7 @@ import { Button, Welcome } from '@storybook/react/demo';
 
 import EditPost from '../components/EditPost';
 import Post from '../components/Post';
+import PostList from '../components/PostList';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -32,6 +33,46 @@ storiesOf('Post', module)
         author="Johnny"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         category="react"
+      />
+    )
+  );
+
+const postList = [
+  {
+    title: 'this is a test',
+    author: 'johnny',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'react',
+    id: '1',
+  },
+  {
+    title: 'this is a test',
+    author: 'johnny',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'react',
+    id: '2',
+  },
+  {
+    title: 'this is a test',
+    author: 'johnny',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'react',
+    id: '3',
+  },
+  {
+    title: 'this is a test',
+    author: 'johnny',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'react',
+    id: '4',
+  },
+];
+
+storiesOf('PostList', module)
+  .add('default', () =>
+    (
+      <PostList
+        posts={postList}
       />
     )
   );
